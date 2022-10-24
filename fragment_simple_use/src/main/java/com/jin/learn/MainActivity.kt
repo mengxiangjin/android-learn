775f30959bf02ae67ce56404df23091b5ce3a634
zhaojin <zhaojin@top-stack.com>
2022-10-24 17:24:03 +0800

    feat |> fragment生命周期

--- fragment_simple_use/src/main/java/com/jin/learn/MainActivity.kt
+++ fragment_simple_use/src/main/java/com/jin/learn/MainActivity.kt
@@ -7,7 +7,19 @@ import com.jin.learn.databinding.ActivityMainBinding
 import com.jin.learn.fragment.AnotherFragment
 import com.jin.learn.fragment.LeftFragment
 import com.jin.learn.fragment.RightFragment
-
+/*
+* RightFragment 生命周期
+*  1.存在addToBackStack回退栈时：
+*       刚进入MainActivity执行replaceFragment （RightFragment） onAttach -> onCreate -> onCreateView -> onViewCreated -> onActivityCreated -> onStart -> onResume
+*       点击按钮replaceFragment（AnotherFragment）  onPause -> onStop -> onDestroyView (并没有onDestroy  存在addToBackStack回退栈)
+*       回退  onCreateView -> onViewCreated -> onActivityCreated -> onStart -> onResume
+*       再次回退   onPause -> onStop -> onDestroyView -> onDestroy -> onDetach
+*
+* 2.不存在addToBackStack回退栈时：
+*       刚进入MainActivity执行replaceFragment （RightFragment） onAttach -> onCreate -> onCreateView -> onViewCreated -> onActivityCreated -> onStart -> onResume
+*       点击按钮replaceFragment（AnotherFragment）  onPause -> onStop -> onDestroyView -> onDestroy -> onDetach
+*       回退  退出了应用程序
+* */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -36,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         //要替换的布局ID （ViewGroup）
         transaction.replace(R.id.right_fragment,fragment)
         //将此次操作加入到fragment回退栈中(back时是否回退fragment)，参数无意义，一般为null
-        transaction.addToBackStack(null)
+        transaction.addToBackStack(null)  // add
         transaction.commit()
     }
 }
