bf117a99edcc07008873cc4e5f6954377a19cd36
jin <mengxiangjin800@163.com>
2023-04-18 14:28:02 +0800

    feat |> custom

--- my_view/src/main/java/com/jin/my_view/MainActivity.kt
+++ my_view/src/main/java/com/jin/my_view/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.jin.my_view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.jin.my_view.view.InputNumberView
+import android.widget.Button
+
 
 class  MainActivity : AppCompatActivity() {
 
-    private var numberView: InputNumberView? = null
 
     companion object {
         private const val TAG = "MainActivity"
@@ -18,9 +18,9 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        numberView = findViewById(R.id.input_view)
-        numberView?.setNumberChangeListener {value->
-            Log.d(TAG, "onCreate: " + value)
+        findViewById<Button>(R.id.to_login).setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
