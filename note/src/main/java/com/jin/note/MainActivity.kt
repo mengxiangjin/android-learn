e4ec5c91d348126bfd27e792c530606d85cbbb10
unknown <1435145737@qq.com>
2024-11-17 21:46:13 +0800

    feat |> cropView

--- note/src/main/java/com/jin/note/MainActivity.kt
+++ note/src/main/java/com/jin/note/MainActivity.kt
@@ -1,45 +1,127 @@
 package com.jin.note
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
-import android.view.MotionEvent
+import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnTouchListener
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.jin.note.bean.InsertableImg
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import com.jin.note.databinding.ActivityMainBinding
+import com.jin.note.surface.BitmapCropView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var selectPictureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),object : ActivityResultCallback<ActivityResult> {
-        override fun onActivityResult(result: ActivityResult) {
-            result.data?.let { it ->
-                it.data?.let {uri->
-                    val openInputStream = contentResolver.openInputStream(uri)
-                    val insertableImg = InsertableImg(BitmapFactory.decodeStream(openInputStream))
-                    binding.surfaceView.addInsertableObject(insertableImg)
-                }
-            }
-            Log.d("TAG", "onActivityResult: " + result.data?.data)
-        }
-    })
+    private var imgResourseId = mutableListOf<Int>()
 
+//    private var selectPictureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),object : ActivityResultCallback<ActivityResult> {
+//        override fun onActivityResult(result: ActivityResult) {
+//            result.data?.let { it ->
+//                it.data?.let {uri->
+//                    val openInputStream = contentResolver.openInputStream(uri)
+//                    val insertableImg = InsertableImg(BitmapFactory.decodeStream(openInputStream))
+//                    binding.surfaceView.addInsertableObject(insertableImg)
+//                }
+//            }
+//            Log.d("TAG", "onActivityResult: " + result.data?.data)
+//        }
+//    })
+
+    private var rootView = mutableListOf<View>()
+    private var currentShowIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        imgResourseId.add(R.drawable.img)
+        imgResourseId.add(R.drawable.img_1)
+        imgResourseId.add(R.drawable.img_2)
 
-//        binding.llImg.setOnClickListener {
+        imgResourseId.forEach {
+            val view = LayoutInflater.from(this).inflate(R.layout.item_test, binding.clContent, false)
+            val imgView = view.findViewById<ImageView>(R.id.img)
+            var cropView = view.findViewById<BitmapCropView>(R.id.crop_view)
+            rootView.add(view)
+            imgView.setImageResource(it)
+            binding.clContent.addView(view)
+        }
+
+        rootView.forEachIndexed { index, view ->
+            view.isInvisible = index != currentShowIndex
+        }
+
+        binding.btnNext.setOnClickListener {
+            currentShowIndex += 1
+            if (currentShowIndex >= rootView.size) {
+                //结束
+                currentShowIndex = rootView.size - 1
+            }
+            rootView.forEachIndexed { index, view ->
+                view.isInvisible = index != currentShowIndex
+            }
+        }
+
+        binding.btnBefore.setOnClickListener {
+            currentShowIndex -=1
+            if (currentShowIndex < 0) {
+                currentShowIndex = 0
+            }
+            rootView.forEachIndexed { index, view ->
+                view.isInvisible = index != currentShowIndex
+            }
+        }
+
+        binding.btnFinish.setOnClickListener {
+            rootView.forEach {
+                val cropView = it.findViewById<BitmapCropView>(R.id.crop_view)
+                val imgView = it.findViewById<ImageView>(R.id.img)
+
+                val cropRegion = cropView.getCropRegion()
+
+                val bitmap = (imgView.drawable as BitmapDrawable).bitmap
+                val createBitmap = Bitmap.createBitmap(
+                    bitmap,
+                    cropRegion!!.left,
+                    cropRegion.top,
+                    cropRegion.width(),
+                    cropRegion.height()
+                )
+                Log.d("zyz", "onCreate: ")
+            }
+        }
+
+
+//        binding.btnCrop.setOnClickListener {
+//            if (binding.img.drawable is BitmapDrawable) {
+//                val cropRegion = binding.cropView.getCropRegion()
+//                val bitmap = (binding.img.drawable as BitmapDrawable).bitmap
+//
+//                Log.d("zyz", "onCreate:left " + cropRegion!!.left)
+//                Log.d("zyz", "onCreate:top " + cropRegion.top)
+//                Log.d("zyz", "onCreate:right " + cropRegion.right)
+//                Log.d("zyz", "onCreate:bottom " + cropRegion.bottom)
+//                Log.d("zyz", "onCreate:width " + bitmap.width)
+//                Log.d("zyz", "onCreate:height " + bitmap.height)
+//
+//
+//                val createBitmap = Bitmap.createBitmap(
+//                    bitmap,
+//                    cropRegion.left,
+//                    cropRegion.top,
+//                    cropRegion.width(),
+//                    cropRegion.height()
+//                )
+//                binding.img2.setImageBitmap(createBitmap)
+//
+//            }
+    }
+
+    //        binding.llImg.setOnClickListener {
 //            if (PermissionUtils.hasPermission(this, listOf(Manifest.permission.READ_MEDIA_IMAGES))) {
 //                Log.d("zyz", "onCreate: " + 1)
 //                val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
@@ -50,46 +132,44 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
 //
-        var lastX = 0f
-        binding.llTools.setOnTouchListener(object : OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent): Boolean {
-                Log.d("zyz", "onTouch: " + event.action)
-                when(event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        lastX = event.x
-                    }
-                    MotionEvent.ACTION_MOVE -> {
-                        var currentX = event.x - lastX
-                        if (currentX > 0) {
-                            //右滑
-                            if (binding.llTools.scrollX <= 0) {
-                                currentX = 0f
-                                binding.llTools.scrollTo(0,0)
-                                lastX = event.x
-                                return true
-                            }
-                        } else {
-                            //左滑
-                            if (binding.llTools.scrollX >= binding.view.width) {
-                                currentX = 0f
-                            }
-                        }
-                        if (binding.llTools.scrollX - currentX > binding.view.width) {
-                            currentX = (binding.llTools.scrollX - binding.view.width).toFloat()
-                        }
-                        binding.llTools.scrollBy(-currentX.toInt(),0)
-                        lastX = event.x
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        var currentX = event.x - lastX
-                    }
-                }
-
-                return true
-            }
-
-        })
-    }
-
-
+    var lastX = 0f
+//        binding.llTools.setOnTouchListener(object : OnTouchListener {
+//            override fun onTouch(v: View?, event: MotionEvent): Boolean {
+//                Log.d("zyz", "onTouch: " + event.action)
+//                when(event.action) {
+//                    MotionEvent.ACTION_DOWN -> {
+//                        lastX = event.x
+//                    }
+//                    MotionEvent.ACTION_MOVE -> {
+//                        var currentX = event.x - lastX
+//                        if (currentX > 0) {
+//                            //右滑
+//                            if (binding.llTools.scrollX <= 0) {
+//                                currentX = 0f
+//                                binding.llTools.scrollTo(0,0)
+//                                lastX = event.x
+//                                return true
+//                            }
+//                        } else {
+//                            //左滑
+//                            if (binding.llTools.scrollX >= binding.view.width) {
+//                                currentX = 0f
+//                            }
+//                        }
+//                        if (binding.llTools.scrollX - currentX > binding.view.width) {
+//                            currentX = (binding.llTools.scrollX - binding.view.width).toFloat()
+//                        }
+//                        binding.llTools.scrollBy(-currentX.toInt(),0)
+//                        lastX = event.x
+//                    }
+//                    MotionEvent.ACTION_UP -> {
+//                        var currentX = event.x - lastX
+//                    }
+//                }
+//
+//                return true
+//            }
+//
+//        })
 }
+
--- note/src/main/java/com/jin/note/surface/BitmapCropView.kt
+++ note/src/main/java/com/jin/note/surface/BitmapCropView.kt
@@ -5,25 +5,35 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
-import android.graphics.PointF
-import android.graphics.Region
+import android.graphics.Rect
+import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 class BitmapCropView@JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     def: Int = 0
 ) :
-    androidx.appcompat.widget.AppCompatImageView(context, attributeSet, def) {
+    View(context, attributeSet, def) {
 
 
         private var circleRadius = 8f
-        private var circleStrokeRadius = 12f
 
 
-    private var controlPointF = mutableListOf<PointF>()
+    private var controlPointF = mutableListOf<CropPoint>()
+
+    private var cropRectF = RectF()
+    private var cropLocationRectF = RectF()
 
     private var path = Path()
+
+    private val minWidth = 160f
+    private val minHeight = 160f
 
     private var circlePaint = Paint().apply {
         style = Paint.Style.FILL_AND_STROKE
@@ -42,6 +52,8 @@ class BitmapCropView@JvmOverloads constructor(
         strokeWidth = 3f
     }
 
+    private var srcLayoutParams:SrcLayoutParams?  = null
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawPath(path,linePaint)
@@ -51,28 +63,125 @@ class BitmapCropView@JvmOverloads constructor(
         }
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setPadding((circleRadius).toInt(),
-            (circleRadius).toInt(), (circleRadius).toInt(), (circleRadius).toInt()
-        )
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    }
-
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         initControlPointF(w, h)
     }
 
+    var downCropPoint: CropPoint? = null
+    var lastX = 0f
+    var lastY = 0f
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                downCropPoint = getDownCropPoint(event.x, event.y)
+                if (downCropPoint != null) {
+                    //有效
+                    lastX = event.rawX
+                    lastY = event.rawY
+                    return true
+                } else {
+                    return false
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val offsetX = event.rawX - lastX
+                val offsetY = event.rawY - lastY
+
+                if (srcLayoutParams == null) return false
+
+                val newLeft = if (left + offsetX < srcLayoutParams!!.left) {
+                    srcLayoutParams!!.left
+                } else if (right - (left + offsetX) < minWidth) {
+                    right - minWidth
+                } else {
+                    left + offsetX
+                }
+                val newTop = if (top + offsetY < srcLayoutParams!!.top) {
+                    srcLayoutParams!!.top
+                } else if (bottom - (top + offsetY) < minHeight) {
+                    bottom - minHeight
+                }else {
+                    top + offsetY
+                }
+
+                val newRight = if (right + offsetX > srcLayoutParams!!.right) {
+                    srcLayoutParams!!.right
+                } else if (right + offsetX - left < minWidth) {
+                    left + minWidth
+                }else {
+                    right + offsetX
+                }
+
+                val newBottom = if (bottom + offsetY > srcLayoutParams!!.bottom) {
+                    srcLayoutParams!!.bottom
+                }else if (bottom + offsetY - top < minHeight) {
+                    top + minHeight
+                } else {
+                    bottom + offsetY
+                }
+
+                when(downCropPoint!!.dragEffectEdge) {
+                    0 -> {
+                        layout(newLeft.toInt(), newTop.toInt(),right,bottom)
+                    }
+                    1 -> {
+                        Log.d("zyz", "onTouchEvent: " + newTop)
+                        layout(left,newTop.toInt(),right,bottom)
+                    }
+                    2 -> {
+                        layout(left,newTop.toInt(),newRight.toInt(),bottom)
+                    }
+                    3 -> {
+                        layout(left,top,newRight.toInt(),bottom)
+                    }
+                    4 -> {
+                        layout(left,top,newRight.toInt(),newBottom.toInt())
+                    }
+                    5 -> {
+                        layout(left,top,right,newBottom.toInt())
+                    }
+                    6 -> {
+                        layout(newLeft.toInt(),top,right,newBottom.toInt())
+                    }
+                    7 -> {
+                        layout(newLeft.toInt(),top,right,bottom)
+                    }
+                }
+                lastX = event.rawX
+                lastY = event.rawY
+            }
+            MotionEvent.ACTION_UP -> {
+            }
+        }
+        return true
+    }
+
     private fun initControlPointF(w: Int, h: Int) {
+        if (srcLayoutParams == null) {
+            srcLayoutParams = SrcLayoutParams(left,top,right,bottom)
+        }
+
         path.reset()
-        controlPointF.add(PointF(paddingLeft.toFloat(), paddingTop.toFloat()))
-        controlPointF.add(PointF(w / 2f, paddingTop.toFloat()))
-        controlPointF.add(PointF(w.toFloat() - paddingEnd, paddingTop.toFloat()))
-        controlPointF.add(PointF(w.toFloat() - paddingEnd, h / 2f))
-        controlPointF.add(PointF(w.toFloat() - paddingEnd, h.toFloat() - paddingBottom))
-        controlPointF.add(PointF(w / 2f, h.toFloat() - paddingBottom))
-        controlPointF.add(PointF(paddingLeft.toFloat(), h.toFloat() - paddingBottom))
-        controlPointF.add(PointF(paddingLeft.toFloat(), h / 2f))
+        controlPointF.clear()
+        cropRectF.set(paddingLeft.toFloat(),paddingTop.toFloat(),w.toFloat() - paddingEnd,h.toFloat() - paddingBottom)
+
+        if (cropLocationRectF.isEmpty) {
+            cropLocationRectF.set(0f,0f, (width - paddingLeft - paddingEnd).toFloat(),
+                (height - paddingTop - paddingBottom).toFloat()
+            )
+        }
+        cropRectF.set(paddingLeft.toFloat(),paddingTop.toFloat(),w.toFloat() - paddingEnd,h.toFloat() - paddingBottom)
+
+
+        controlPointF.add(CropPoint(cropRectF.left, cropRectF.top,0))
+        controlPointF.add(CropPoint(cropRectF.left + cropRectF.width() / 2f, cropRectF.top,1))
+        controlPointF.add(CropPoint(cropRectF.right, cropRectF.top,2))
+        controlPointF.add(CropPoint(cropRectF.right, cropRectF.height() / 2f,3))
+        controlPointF.add(CropPoint(cropRectF.right, cropRectF.bottom,4))
+        controlPointF.add(CropPoint(cropRectF.left + cropRectF.width() / 2f,  cropRectF.bottom,5))
+        controlPointF.add(CropPoint( cropRectF.left,  cropRectF.bottom,6))
+        controlPointF.add(CropPoint( cropRectF.left, cropRectF.height() / 2f,7))
         controlPointF.forEachIndexed { index, pointF ->
             if (index == 0) {
                 path.moveTo(pointF.x,pointF.y)
@@ -82,6 +191,45 @@ class BitmapCropView@JvmOverloads constructor(
         }
         path.close()
     }
+
+    private fun getDownCropPoint(x: Float,y: Float): CropPoint? {
+        controlPointF.forEachIndexed { index, cropPoint ->
+            val distance = sqrt((cropPoint.x - x) * (cropPoint.x - x) + (cropPoint.y - y) * (cropPoint.y - y))
+            if (distance <= circleRadius) {
+                return cropPoint
+            }
+        }
+        return null
+    }
+
+    fun getCropRegion(): Rect? {
+        if (srcLayoutParams != null) {
+            val realRight = if (srcLayoutParams!!.right - right <= 0) {
+                srcLayoutParams!!.right - srcLayoutParams!!.left - paddingLeft - paddingEnd
+            } else {
+                right - srcLayoutParams!!.left - paddingLeft - paddingEnd
+            }
+
+            val realBottom =  if (srcLayoutParams!!.bottom - bottom <= 0) {
+                srcLayoutParams!!.bottom - srcLayoutParams!!.top - paddingTop - paddingBottom
+            } else {
+                bottom - srcLayoutParams!!.top - paddingTop - paddingBottom
+            }
+            val rect = Rect(left - srcLayoutParams!!.left,
+                abs( top - srcLayoutParams!!.top) ,realRight,realBottom)
+            if (rect.isEmpty) {
+                return Rect(0,0,width - paddingLeft - paddingEnd,height - paddingTop - paddingBottom)
+            }
+            return rect
+        }
+        return null
+    }
+
+
+    class CropPoint(var x:Float,var y :Float,var dragEffectEdge :Int) {
+    }
+
+    class SrcLayoutParams(var left: Int,var top: Int,var right: Int,var bottom: Int){}
 
 
 
